38e7d72cc25ed84e808c08c3e8a8a961332c885c
Priscila <priscilalima07@gmail.com>
2021-04-17 10:12:18 +0100

    Use Helper to add default name store

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import {getFunName} from '../helpers';
 class StorePicker extends React.Component {
   render() {
+    console.log(this);
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
 
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store → </button>
       </form>
     );
