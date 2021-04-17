8f9fa9fc15072ab2308d9e4994834a6375a12b5a
Priscila <priscilalima07@gmail.com>
2021-04-17 16:55:52 +0100

    Add ability to update fishes on inventory

--- src/components/App.js
+++ src/components/App.js
@@ -42,6 +42,12 @@ class App extends React.Component {
     });
   };
 
+  updateFish = (key, updatedFish) => {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({fishes});
+  };
+
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes});
   };
@@ -71,7 +77,9 @@ class App extends React.Component {
         <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
         />
       </div>
     );
---
+++ src/components/EditFishForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class EditFishForm extends React.Component {
+  handleChange = e => {
+    const updatedFish = {
+      ...this.props.fish,
+      [e.currentTarget.name]: e.currentTarget.value
+    };
+
+    this.props.updateFish(this.props.index, updatedFish);
+  };
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+          type="text"
+        />
+        <input
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+          type="text"
+        />
+        <select
+          name="status"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea
+          name="desc"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        ></textarea>
+        <input
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+          type="text"
+        />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
