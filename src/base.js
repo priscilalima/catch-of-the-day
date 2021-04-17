3b5fd79919bac669c09cce0026b0d34d4d4d58ab
Priscila <priscilalima07@gmail.com>
2021-04-17 16:00:03 +0100

    Add firebase and start to sync state with database

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyDxgh38bmUhjB-_CtgltccWrQPfWF11BIk',
+  authDomain: 'catch-of-the-day-108ad.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-108ad-default-rtdb.firebaseio.com'
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export {firebaseApp};
+
+export default base;
